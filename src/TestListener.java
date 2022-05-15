3521762c89d9551d9a4c7ef5b9d05f841c10730d
Dol3v <dol12dab@gmail.com>
2022-05-16 00:11:17 +0300

    removed debug HitListener

--- src/TestListener.java
+++
@@ -1,7 +0,0 @@
-public class TestListener implements HitListener {
-
-    @Override
-    public void hitEvent(Block beingHit, Ball hitter) {
-        System.out.println("Block " + beingHit + " was hit by " + hitter);
-    }
-}
