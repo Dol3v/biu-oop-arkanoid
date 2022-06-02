5c101f73c4b9c7a4452b06f820a8c5e0fa133e56
Dol3v <dol12dab@gmail.com>
2022-06-02 17:53:28 +0300

    added GameFlow

--- src/Ass6Game.java
+++ src/Ass6Game.java
@@ -1,6 +1,15 @@
 // Dolev Dabush, id: 215372939
 
+import biuoop.GUI;
+import game.Animation;
+import game.AnimationRunner;
 import game.GameLevel;
+import levels.DirectHitLevel;
+import levels.LevelInformation;
+import utils.Consts;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Assignment: initializes and runs the game.
@@ -13,8 +22,14 @@ public class Ass6Game {
      * @param args command-line arguments
      */
     public static void main(String[] args) {
-        GameLevel gameLevel = new GameLevel();
-        gameLevel.initialize();
-        gameLevel.run();
+        GUI gui = new GUI("arkanoid", Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT);
+        AnimationRunner runner = new AnimationRunner(gui);
+        GameFlow flow = new GameFlow(gui.getKeyboardSensor(), runner);
+
+        List<LevelInformation> levels = new ArrayList<>();
+        levels.add(new DirectHitLevel());
+        flow.runLevels(levels);
+
+        gui.close();
     }
 }
---
+++ src/GameFlow.java
@@ -0,0 +1,36 @@
+import biuoop.KeyboardSensor;
+import game.AnimationRunner;
+import game.GameLevel;
+import levels.LevelInformation;
+import utils.Counter;
+
+import java.util.List;
+
+/**
+ * Runs several game levels until the game ends.
+ */
+public class GameFlow {
+
+    private KeyboardSensor keyboardSensor;
+    private AnimationRunner runner;
+    private Counter score;
+
+    public GameFlow(KeyboardSensor keyboardSensor, AnimationRunner runner) {
+        this.keyboardSensor = keyboardSensor;
+        this.runner = runner;
+        this.score = new Counter(0);
+    }
+
+    public void runLevels(List<LevelInformation> levels) {
+        for (LevelInformation level : levels) {
+            GameLevel gameLevel = new GameLevel(level, keyboardSensor, runner, score);
+            gameLevel.initialize();
+
+            gameLevel.run();
+
+            if (gameLevel.getAvailableBalls().getValue() == 0) {
+                break;
+            }
+        }
+    }
+}
