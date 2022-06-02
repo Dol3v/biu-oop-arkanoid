d97dd2e93a7b970bc2274b2f5cc5f35b930bcf0a
Dol3v <dol12dab@gmail.com>
2022-06-02 21:13:49 +0300

    added command-line argument support

--- src/Ass6Game.java
+++ src/Ass6Game.java
@@ -18,20 +18,27 @@ public class Ass6Game {
     /**
      * Initializes and runs the game.
      *
-     * @param args command-line arguments
+     * @param args command-line arguments; each specifies the number of a level to be run
      */
     public static void main(String[] args) {
         GUI gui = new GUI("arkanoid", Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT);
         AnimationRunner runner = new AnimationRunner(gui);
         GameFlow flow = new GameFlow(gui.getKeyboardSensor(), runner);
 
-        List<LevelInformation> levels = new ArrayList<>();
-        levels.add(new DirectHitLevel());
-//        levels.add(new WideEasyLevel());
-//        levels.add(new Green3Level());
-        levels.add(new FinalFourLevel());
-        flow.runLevels(levels);
+        LevelInformation[] levelList = {new DirectHitLevel(), new WideEasyLevel(), new Green3Level(),
+                new FinalFourLevel()};
 
+        List<LevelInformation> levels = new ArrayList<>();
+
+        for (String arg : args) {
+            try {
+                int level = Integer.parseInt(arg);
+                levels.add(levelList[level - 1]);
+            } catch (NumberFormatException | IndexOutOfBoundsException ignored) {
+            }
+        }
+
+        flow.runLevels(levels);
         gui.close();
     }
 }
